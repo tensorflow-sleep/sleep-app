5a0af71741e01a1bee1426ff994864d37acbebb6
Park JiHye <youo78@naver.com>
2018-08-12 19:59:23 +0900

    블루투스 통신

--- app/src/main/java/com/example/pjh/tensorflow_sleep/LeftFragment.java
+++ app/src/main/java/com/example/pjh/tensorflow_sleep/LeftFragment.java
@@ -7,14 +7,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 /*
 이산화탄소의 농도를 보여주는 Fragment
  */
 public class LeftFragment extends Fragment {
 
+    TextView textView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.left_fragment, container, false);
+        View view =  inflater.inflate(R.layout.left_fragment, container, false);
+        Bundle bundle = this.getArguments();
+        String data = "";
+        if(bundle != null)
+            data = bundle.getString("data","0");
+        textView = (TextView)view.findViewById(R.id.textView);
+        textView.setText(data);
+
+        return view;
+    }
+
+    public void setText(){
+        Bundle bundle = this.getArguments();
+        String data = "";
+        if(bundle != null)
+            data = bundle.getString("data","0");
+
+        textView.setText(data);
     }
 }
